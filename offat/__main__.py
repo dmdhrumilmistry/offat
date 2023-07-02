ab819aef4d6b075c05ffda66bfef5fee3cd49b24
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-07-02 17:28:39 +0530

    load openapi specs
    extract endpoints and supported HTTP methods
    
    create tester package

--- offat/__main__.py
+++ offat/__main__.py
@@ -1,3 +1,12 @@
-from .utils import read_yaml
+from argparse import ArgumentParser
+from .openapi import OpenAPIParser
 
-print(read_yaml('conf.yaml'))
+
+if __name__ == '__main__':
+    parser = ArgumentParser(prog='offat')
+    parser.add_argument('-f','--file', dest='fpath', type=str, help='path of openapi/swagger specification file', required=True)
+    
+    args = parser.parse_args()
+
+    api_doc = OpenAPIParser(args.fpath)
+    print(api_doc.base_url)
---
+++ offat/openapi.py
@@ -0,0 +1,35 @@
+#from urllib.parse import urljoin
+from prance import ResolvingParser
+# from pprint import pprint as print
+from .logger import create_logger
+
+
+logger = create_logger(__name__)
+
+
+class OpenAPIParser:
+    ''''''
+    def __init__(self, fpath:str) -> None:
+        self._parser = ResolvingParser(fpath, backend = 'openapi-spec-validator')
+        if self._parser.valid:
+            logger.info('Specification file is valid')
+        else:
+            logger.error('Specification file is invalid!')
+
+        self._spec = self._parser.specification
+        self.base_url = f"http://{self._spec.get('host')}{self._spec.get('basePath','')}"
+
+
+    def _get_endpoints(self):
+        '''Returns list of endpoint paths along with '''
+        endpoints = []
+
+        for endpoint in self._spec.get('paths', {}).keys():
+            methods = list(self._spec['paths'][endpoint].keys())
+            methods.remove('parameters')
+            endpoints.append((endpoint, methods))
+
+        return endpoints
+
+    
+
