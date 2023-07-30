37051c1aac6477ab4289f8deba13e215298b8f3d
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-07-30 17:24:09 +0530

    refactor code

--- offat/tester/data_exposure.py
+++ offat/tester/data_exposure.py
@@ -1,6 +1,6 @@
+from re import findall
 from .regexs import sensitive_data_regex_patterns
 
-import re
 
 
 def detect_data_exposure(data:str)->dict:
@@ -17,7 +17,7 @@ def detect_data_exposure(data:str)->dict:
     detected_exposures = {}
 
     for pattern_name, pattern in sensitive_data_regex_patterns.items():
-        matches = re.findall(pattern, data)
+        matches = findall(pattern, data)
         if matches:
             detected_exposures[pattern_name] = matches
 
@@ -25,11 +25,10 @@ def detect_data_exposure(data:str)->dict:
 
 
 if __name__ == '__main__':
-    sample_test_data = '''
-    {
-        "message":"Please do not share your AWS Access Key: AKIAEXAMPLEKEY, AWS Secret Key: 9hsk24mv8wzJ3/78mx3p5x3E7N0P39n6Zq0RxTee, Aadhaar: 1234 5678 9012, PAN: ABCDE1234F, SSN: 123-45-6789, credit card: 1234-5678-9012-3456, or email: john.doe@example.com. You can reach me at +1 (555) 123-4567 or via email at contact@example.com. The event date is scheduled for 01/25/2023. The server IP is 192.168.1.1, and IPv6 is 2001:0db8:85a3:0000:0000:8a2e:0370:7334. Password examples: Passw0rd!, Strong@123, mySecret12#. My VISA Card: 4001778837951872, "
-    }
-    '''
+    from json import dumps
+    sample_test_data = dumps({
+        "message" : "Please do not share your AWS Access Key: AKIAEXAMPLEKEY, AWS Secret Key: 9hsk24mv8wzJ3/78mx3p5x3E7N0P39n6Zq0RxTee, Aadhaar: 1234 5678 9012, PAN: ABCDE1234F, SSN: 123-45-6789, credit card: 1234-5678-9012-3456, or email: john.doe@example.com. You can reach me at +1 (555) 123-4567 or via email at contact@example.com. The event date is scheduled for 01/25/2023. The server IP is 192.168.1.1, and IPv6 is 2001:0db8:85a3:0000:0000:8a2e:0370:7334. Password examples: Passw0rd!, Strong@123, mySecret12#. My VISA Card: 4001778837951872"
+    })
 
     # detect data exposure
     exposures = detect_data_exposure(sample_test_data)
